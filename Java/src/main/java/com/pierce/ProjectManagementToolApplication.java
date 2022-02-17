401fb52390a7142c985b090770c301291f51ac69
PierceBeckett <pierceabeckett@gmail.com>
2022-02-17 16:19:23 -0500

    merged ehcache, log4j2, lombok

--- Java/src/main/java/com/pierce/ProjectManagementToolApplication.java
+++ Java/src/main/java/com/pierce/ProjectManagementToolApplication.java
@@ -1,19 +1,14 @@
 package com.pierce;
 
-import java.util.Arrays;
-
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 
 @SpringBootApplication
+@EnableCaching
 public class ProjectManagementToolApplication {
 
 	public static void main(String[] args) {
