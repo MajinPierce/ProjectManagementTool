8b07463eca29a9d04c59e5b4f3dd98ce43bddb26
Justin Gaddy <justingaddy1@gmail.com>
2022-02-17 13:46:09 -0500

    added project lombok

--- Java/src/main/java/com/pierce/domain/User.java
+++ Java/src/main/java/com/pierce/domain/User.java
@@ -1,30 +1,20 @@
 package com.pierce.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.PrePersist;
-import javax.persistence.PreUpdate;
-import javax.persistence.Transient;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 @Entity
+@Data
 public class User implements UserDetails{
 
 	@Id
@@ -54,73 +44,8 @@ public class User implements UserDetails{
 	protected void onUpdate() { this.updatedAt = new Date(); }
 	
 	//OneToMany with Project
-	public User() {
-	}
-	
-	public Long getId() {
-		return this.id;
-	}
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
-	public String getUsername() {
-		return this.username;
-	}
-	
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	public String getFullName() {
-		return this.fullName;
-	}
-	
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
-	
-	public String getPassword() {
-		return this.password;
-	}
-	
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public String getConfirmPassword() {
-		return this.confirmPassword;
-	}
-	
-	public void setConfirmPassword(String confirmPassword) {
-		this.confirmPassword = confirmPassword;
-	}
-	
-	public Date getCreatedAt() {
-		return this.createdAt;
-	}
-	
-	public void setCreatedAt(Date createdAt) {
-		this.createdAt = createdAt;
-	}
-	
-	public Date getUpdatedAt() {
-		return this.updatedAt;
-	}
-	
-	public void setUpdatedAt(Date updatedAt) {
-		this.updatedAt = updatedAt;
-	}
-	
-    public List<Project> getProjects() {
-		return this.projects;
-	}
-    
-	public void setProjects(List<Project> projects) {
-		this.projects = projects;
-	}
-	
+	public User() {}
+
 	//User Details
 	@Override
     @JsonIgnore
