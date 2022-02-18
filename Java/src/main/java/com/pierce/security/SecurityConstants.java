badac9cd4e11431fed556f73d9e2b22d29c31788
PierceBeckett <pierceabeckett@gmail.com>
2022-02-18 12:50:38 -0500

    fix security constant

--- Java/src/main/java/com/pierce/security/SecurityConstants.java
+++ Java/src/main/java/com/pierce/security/SecurityConstants.java
@@ -7,5 +7,5 @@ public class SecurityConstants {
 	public static final String TOKEN_PREFIX = "Bearer ";
 	public static final String HEADER_STRING = "Authorization";
 	public static final long EXPIRATION_TIME = 3_000_000;
-	public static final String SECRET = "SecretKeyToGenJWT";
+	public static final String SECRET = System.getenv("SECRET");
 }
