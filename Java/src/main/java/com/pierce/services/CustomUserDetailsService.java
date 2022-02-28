dd3da0d5d1264f3b6971baf0ecf03aaa9c812050
Letholor <lee.reynolds2008@gmail.com>
2022-02-28 16:53:10 -0500

    Exception check

--- Java/src/main/java/com/pierce/services/CustomUserDetailsService.java
+++ Java/src/main/java/com/pierce/services/CustomUserDetailsService.java
@@ -19,14 +19,14 @@ public class CustomUserDetailsService implements UserDetailsService{
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		User user = userRepository.findByUsername(username);
-		if(user == null) { new UsernameNotFoundException("User not found"); }
+		if(user == null) { throw new UsernameNotFoundException("User not found"); }
 		return user;
 	}
 	
 	@Transactional
 	public User loadUserById(Long id) {
 		User user = userRepository.getById(id);
-		if(user == null) { new UsernameNotFoundException("User not found"); }
+		if(user == null) { throw new UsernameNotFoundException("User not found"); }
 		return user;
 	}
 }
